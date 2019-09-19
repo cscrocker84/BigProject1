0a012e030bb1a85e5ceb73712b79d2ea4df1dfb5
Charlie Crocker <cscrocker84@gmail.com>
2019-09-19 11:55:57 -0500

    Got ticketmaster api working on app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,28 @@
 
-var searchParam;
-var keywords = [searchParam.split(" ")];
-var performer = "";
-for(var i = 0; i <= keywords.length; i++)
-{
-    performer = performer + "%20" +keywords[i]
-}
-
-var seatgeekURLbyPerformer = "https://api.seatgeek.com/2/performers?q="+performer+"&client_id=MTg0MDYxODh"
-
-
-  //meetup call 
-  var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://seatgeek-seatgeekcom.p.rapidapi.com/performers",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "seatgeek-seatgeekcom.p.rapidapi.com",
-		"x-rapidapi-key": "d80dc8d8e3msh8a2255600c701e6p1916f8jsn46bcf06e5193"
-	}
-}
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-
-
-var zip = seatgeekResponse.postal_code;
-
-//eventbrite call
-var settings = 
-{
-	"async": true,
-	"crossDomain": true,
-	"url": "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=https://samples.openweathermap.org/data/2.5/weather?zip="+zip+",us&appid=5ea5e37dfb4cfb42bf600406ca2185cc",
-	"method": "GET"
-}
-​
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-
+function pullTicketMaster() {
+	var city = 'Nashville';
+	var apiKey = 'aebAmnmDUA7RQsYCzucwhkP7dqjKFGaH';
+	var apiURL = `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&apikey=${apiKey}`;
+  
+	var options = {
+	  url: apiURL,
+	  method: 'GET'
+	};
+  
+	$.ajax(options).then(function(response) {
+	  // console.log(response);
+	  var data = response._embedded;
+	  var cardImgSrc = data.events[0].images[0].url;
+	  var cardTitle = data.events[0].name;
+  
+	
+  
+  
+	  console.log(response);
+	  console.log(cardTitle);
+	  
+	});
+  }
+  pullTicketMaster();
+  
 
