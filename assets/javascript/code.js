027027193aea524267060eff67b9eb32ae5687ab
Anna Thammakane <anna.thammakane@gmail.com>
2019-09-19 19:09:17 -0500

    new

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,71 +1,44 @@
-$(document).ready(function(){
+var searchParam;
+var keywords = [searchParam.split(" ")];
+var performer = "";
+for(var i = 0; i <= keywords.length; i++)
+{
+    performer = performer + "%20" +keywords[i]
+}
+
+var seatgeekURLbyPerformer = "https://api.seatgeek.com/2/performers?q="+performer+"&client_id=MTg0MDYxODh"
 
 
-	var searchParam;
-	var keywords = [searchParam.split(" ")];
-	var performer = "";
-
-	for(var i = 0; i <= keywords.length; i++)
-	{
-		performer = performer + "%20" +keywords[i]
+  //meetup call 
+  var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://seatgeek-seatgeekcom.p.rapidapi.com/events",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "seatgeek-seatgeekcom.p.rapidapi.com",
+		"x-rapidapi-key": "6d85d63f9bmsh0562612230be246p1a4165jsn885de49510b4"
 	}
+}
 
-	var ticketmasterURLbyPerformer = "https://app.ticketmaster.com/discovery/v2/events.json?keyword="+performer+"&postalCode=37201&apikey=IeNsrAdcreZTvgQH5cJhiO0JA2RqVAWw";
-
-
-  // ticketmaster call ?
- 	 var tmSettings = {
-		type:"GET",
-		url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=IeNsrAdcreZTvgQH5cJhiO0JA2RqVAWw",
-		async:true,
-		dataType: "json",
-		success: function(json) {
-					console.log(json);
-					// Parse the response.
-					// Do other things.
-				 },
-		error: function(xhr, status, err) {
-					// This time, we do not end up here!
-				 }
-	}
-
-	$.ajax(tmSettings).done(function(ticketmasterResponse){
-		console.log(ticketmasterResponse);
-	});
-
-
-
-	// weather call
-	var weatherSettings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=https://samples.openweathermap.org/data/2.5/weather?zip="+zip+",us&appid=5ea5e37dfb4cfb42bf600406ca2185cc",
-		"method": "GET"
-	}
-	​
-	$.ajax(weatherSettings).done(function(response){
-		console.log(response);
-
-	searchParam = $("#searchBar").val().trim();
-
-	});
-
-
-// $.ajax({
-// 	type:"GET",
-// 	url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=IeNsrAdcreZTvgQH5cJhiO0JA2RqVAWw",
-// 	async:true,
-// 	dataType: "json",
-// 	success: function(json) {
-// 				console.log(json);
-// 				// Parse the response.
-// 				// Do other things.
-// 				},
-// 	error: function(xhr, status, err) {
-// 				// This time, we do not end up here!
-// 			 }
-// });
-
-
-
+$.ajax(settings).done(function (seatgeekResponse) {
+	console.log(seatgeekResponse);
 });
+
+
+var zip = seatgeekResponse.postal_code;
+
+//eventbrite call
+var settings = 
+{
+	"async": true,
+	"crossDomain": true,
+	"url": "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=https://samples.openweathermap.org/data/2.5/weather?zip="+zip+",us&appid=5ea5e37dfb4cfb42bf600406ca2185cc",
+	"method": "GET"
+}
+​
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+searchParam = $("#searchBar").val().trim();
